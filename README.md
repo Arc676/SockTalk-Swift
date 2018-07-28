4db8b727512512943663b4c14375f7ead24f4b2a
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-07-28 12:29:16 +0200

    Created project
    Add README and LICENSE

---
+++ README.md
@@ -0,0 +1,7 @@
+# SockTalk (Swift library)
+
+The [SockTalk library](https://github.com/Arc676/SockTalk) is a C++ library for network communication over sockets. Swift, not being a C-family language, cannot be linked to C libraries the way Objective-C can. This project's aim is to create a Cocoa framework written in Swift that conforms to the SockTalk protocol and thus allow the creation of Swift programs capable of communicating with C programs following SockTalk protocol.
+
+## Licensing
+
+Project available under GPLv3. See `LICENSE` for full license text.
---
+++ SockTalk/SockTalk.h
@@ -0,0 +1,32 @@
+//
+//  SockTalk.h
+//  SockTalk
+//
+//  Created by Alessandro Vinciguerra on 28/07/2018.
+//      <alesvinciguerra@gmail.com>
+//Copyright (C) 2018 Arc676/Alessandro Vinciguerra
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation (version 3)
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//See README and LICENSE for more details
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SockTalk.
+FOUNDATION_EXPORT double SockTalkVersionNumber;
+
+//! Project version string for SockTalk.
+FOUNDATION_EXPORT const unsigned char SockTalkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SockTalk/PublicHeader.h>
+
+
