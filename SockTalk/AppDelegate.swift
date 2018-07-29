53a8f285cffc06bede36096a50b65122cfaa8b4d
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-07-29 22:13:56 +0200

    Added separate target
    Renamed folder for library code

---
+++ SockTalk/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SockTalk
+//
+//  Created by Alessandro Vinciguerra on 29/07/2018.
+//  Copyright © 2018 Arc676. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ SockTalk/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SockTalk
+//
+//  Created by Alessandro Vinciguerra on 29/07/2018.
+//  Copyright © 2018 Arc676. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
